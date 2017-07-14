b7ac6d47ad12e45d79a4ac04d42a6d6123930416
Mihaela <mihaela.ivanova.ivanova@gmail.com>
2017-07-14 18:45:30 +0300

    Packages updated, server changes

--- Night-Out/server.js
+++ Night-Out/server.js
@@ -1,8 +1,9 @@
 const app = require('./server/app');
 const browser = require('openurl');
 
-require('./routes/server.routes')(app);
+require('./routes/general.routes')(app);
 require('./routes/users.routes')(app);
+require('./routes/places.routes')(app);
 
 app.listen(3001, () => console.log('Server running at: 3001'));
 browser.open(`http://localhost:3001`);
