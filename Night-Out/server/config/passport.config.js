bed4bdff06a5c3568b3fd6e7c313e1722ff91cf2
2NSoft <nikolay.i.nachev@gmail.com>
2017-07-17 11:32:30 +0300

    password hashing implemented

--- Night-Out/server/config/passport.config.js
+++ Night-Out/server/config/passport.config.js
@@ -5,54 +5,53 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
 const configPassport = (app, data) => {
-    passport.use(new Strategy(
-        (username, password, done) => {
-            return data.users.findOne(username)
-                .then((user) => {
+    passport.use( new Strategy(
+        ( username, password, done ) => {
+            return data.users.findOne( username )
+                .then( ( user ) => {
                     user = user[0];
-                    if (!user) {
-                        return done(null, false,
-                            { message: 'Incorrect username.' });
+                    if ( !user ) {
+                        return done( null, false,
+                            { message: 'Incorrect username.' } );
                     }
-                    if (!data.users.validatePassword(user.password)) {
-                        return done(null, false,
-                            { message: 'Incorrect password.' });
+                    if ( !data.users.validatePassword( user, password ) ) {
+                        return done( null, false,
+                            { message: 'Incorrect password.' } );
                     }
-                    return done(null, user);
-                })
-                .catch((err) => {
-                    return done(err);
-                });
+                    return done( null, user );
+                } )
+                .catch( ( err ) => {
+                    return done( err );
+                } );
         }
-    ));
-    app.use(cookieParser());
-    app.use(session({
+    ) );
+    app.use( cookieParser() );
+    app.use( session( {
         secret: 'Deus ex machina',
-        maxAge: new Date(Date.now() + 60 * 60 * 1000),
+        maxAge: new Date( Date.now() + 60 * 60 * 1000 ),
         store: new MongoStore(
             { url: data.sessionStoreName },
-            (err) => {
-                console.log(err || 'connect-mongodb setup ok');
-            })
-    }));
+            ( err ) => {
+                console.log( err || 'connect-mongodb setup ok' );
+            } ),
+    } ) );
 
-    app.use(passport.initialize());
-    app.use(passport.session());
+    app.use( passport.initialize() );
+    app.use( passport.session() );
 
-    passport.serializeUser((user, done) => {
-        done(null, user._id);
-    });
+    passport.serializeUser( ( user, done ) => {
+        done( null, user._id );
+    } );
 
-    passport.deserializeUser((id, done) => {
-        return data.users.findById(id)
-            .then((user) => {
-                //console.log(user);
+    passport.deserializeUser( ( id, done ) => {
+        return data.users.findById( id )
+            .then( ( user ) => {
                 user = user[0];
-                done(null, user);
-            })
-            .catch(done);
-    });
-    console.log('passport initialized');
+                done( null, user );
+            } )
+            .catch( done );
+    } );
+    console.log( 'passport initialized' );
 };
 
 module.exports = configPassport;
--- Night-Out/server/data/user.data.js
+++ Night-Out/server/data/user.data.js
@@ -1,55 +1,37 @@
-const users = [
-    {
-        username: '2NSoft',
-        password: '2NSoft',
-        id: '1234567890',
-    }];
-
 const BaseData = require('./base.data.js');
+const crypto = require('crypto');
+const { ObjectID } = require('mongodb');
+
+const hashPassword = (password) => {
+    return crypto.createHmac('sha256', 'Deus ex machina')
+        .update(password)
+        .digest('hex');
+};
 
 class User extends BaseData {
-    constructor( db ) {// , { username, password, email, stringProfilePicture, id }) {
+    constructor( db ) {
         super(db, User);
-        // this.username = username;
-        // this.password = password;
-        // this.email = email;
-        // this.stringProfilePicture = stringProfilePicture;
-        // this.id = id;
+    }
+
+    create(model) {
+        model.password = hashPassword(model.password);
+        super.create(model);
     }
 
     findOne(username) {
         return this.getAll({
             username: { $eq: username },
         });
-        //return new Promise( (resolve, reject ) => {
-        //    const user = users.find( (u) => {
-        //        return u.username === username;
-        //    });
-        //    if (!user) {
-        //        reject();
-        //    }
-        //    resolve(new User(/* this.db, */user));
-        //});
     }
     findById(id) {
-        //console.log(id);
-        id = require('mongodb').ObjectID(id);
+        id = new ObjectID(id);
         return this.getAll({
             _id: { $eq: id },
         });
-        //return new Promise((resolve, reject) => {
-        //    const user = users.find( (u) => {
-        //        return u.id === id;
-        //    });
-        //    if (!user) {
-        //        reject();
-        //    }
-        //    resolve(new User(/* this.db, */user));
-        //});
     }
 
-    validatePassword(password) {
-        return true;//this.password === password;
+    validatePassword(user, password) {
+        return hashPassword(password) === user.password;
     }
 }
 
