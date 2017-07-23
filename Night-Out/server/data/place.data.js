a24b4ccf1bc6df0e94e5e822c0e88a804c4660db
2NSoft <nikolay.i.nachev@gmail.com>
2017-07-23 14:30:38 +0300

    towns added to the database

--- Night-Out/server/data/place.data.js
+++ Night-Out/server/data/place.data.js
@@ -5,6 +5,11 @@ class Places extends BaseData {
     constructor( db ) {
         super(db, Place, Place);
     }
+
+    create(model, towns) {
+        towns.append(model.town);
+        return super.create(model);
+    }
 }
 
 module.exports = Places;
