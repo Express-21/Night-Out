08b6388bf2db3088d220618bd4a6c0c246c24801
Mihaela <mihaela.ivanova.ivanova@gmail.com>
2017-07-23 19:22:14 +0300

    minor changes

--- Night-Out/server/app/routes/api.routes.js
+++ Night-Out/server/app/routes/api.routes.js
@@ -1,8 +1,8 @@
 const attach = (app, data) => {
-    app.get('/api/logged', (res,req) => {
+    app.get('/api/logged', (res, req) => {
         const username = req.user.username || null;
         req.send( username );
     });
-}
+};
 
 module.exports = attach;
