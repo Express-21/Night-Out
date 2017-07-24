4b0d9a046accb464179e6e998dcd68d3b7fdf9a0
Mihaela <mihaela.ivanova.ivanova@gmail.com>
2017-07-24 20:50:12 +0300

    User register public validation added

---
+++ Night-Out/public/scripts/validations.js
@@ -0,0 +1,13 @@
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm_password');
+
+function validatePassword() {
+  if (password.value !== confirmPassword.value) {
+    confirmPassword.setCustomValidity('Passwords do not match');
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
