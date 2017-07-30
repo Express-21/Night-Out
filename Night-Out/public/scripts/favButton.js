13b601764e5fdcb6a317716de468861f4867cb71
Mihaela <mihaela.ivanova.ivanova@gmail.com>
2017-07-30 22:10:48 +0300

    favourites try

---
+++ Night-Out/public/scripts/favButton.js
@@ -0,0 +1,25 @@
+/* global $ */
+
+$(document).ready(function() {
+
+    $('#fav').click(function(ev) {
+        $(this).toggleClass('added');
+    });
+
+    const placeId = window.location.pathname.split('/').pop();
+    fetch('/api/v1/favourites')
+        .then((res) => {
+            return res.json({});
+        })
+        .then((fav) => {
+            if (fav.indexOf(placeId) === -1) {
+                $('#fav').removeClass('added');
+            }
+            
+            else {
+                $('#fav').addClass('added');
+            }
+        });
+});
+
+
