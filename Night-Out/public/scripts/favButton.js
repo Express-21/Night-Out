443e56981976073d7e0b7c39324f9b3dd29bfd3b
Mihaela <mihaela.ivanova.ivanova@gmail.com>
2017-07-30 23:41:32 +0300

    add and delete favourite place fi=unctionality

--- Night-Out/public/scripts/favButton.js
+++ Night-Out/public/scripts/favButton.js
@@ -1,24 +1,54 @@
 /* global $ */
 
 $(document).ready(function() {
+const placeId = window.location.pathname.split('/').pop();
+    function checkIfAdded() {
+        fetch('/api/v1/favourites', { credentials: 'include' })
+            .then((res) => {
+                return res.json({});
+            })
+            .then((fav) => {
+                if (fav.indexOf(placeId) === -1) {
+                    fetch('/api/v1/favourites',
+                        {
+                            head: 'POST',
+                            body: { placeId },
+                            credentials: 'include',
+                        });
+                    $('#fav').attr('class', '');
+                }
+                else {
+                $('#fav').attr('class', 'added');
+                }
+            });
+    }
+
+    window.onload = checkIfAdded;
 
     $('#fav').click(function(ev) {
-        $(this).toggleClass('added');
+        fetch('/api/v1/favourites', { credentials: 'include' })
+            .then((res) => {
+                return res.json({});
+            })
+            .then((fav) => {
+                if (fav.indexOf(placeId) === -1) {
+                    fetch('/api/v1/favourites',
+                        {
+                            head: 'POST',
+                            body: { placeId },
+                            credentials: 'include',
+                        });
+                    $('#fav').attr('class', 'added');
+                }
+                else {
+                    fetch('/api/v1/favourites', {
+                        head: 'DELETE',
+                        credentials: 'include',
+                    });
+                }
+                $('#fav').attr('class', '');
+            });
     });
-
-    const placeId = window.location.pathname.split('/').pop();
-    fetch( '/api/v1/favourites', { credentials: 'include' } )
-        .then((res) => {
-            return res.json({});
-        })
-        .then((fav) => {
-            if (fav.indexOf(placeId) === -1) {
-                $('#fav').removeClass('added');
-            }
-            else {
-                $('#fav').addClass('added');
-            }
-        });
 });
 
 
