c9830dbfbce1aeff3cf7f5f2e119a7fc4fbaf105
2NSoft <nikolay.i.nachev@gmail.com>
2017-07-31 21:44:12 +0300

    place search done

--- Night-Out/public/scripts/searchByPlace.js
+++ Night-Out/public/scripts/searchByPlace.js
@@ -7,6 +7,22 @@ fetch( '/api/v1/places' )
         $( '#search' ).typeahead( { source: names } );
     } );
 
-$('.btn-search').click(function(ev) {
+$( '.btn-search' ).click( () => {
+    const name = $( '#search' ).val();
+    const town = $( '#town-select' ).val();
 
+    $( '.place-container' ).each( ( index, element ) => {
+        const placeTitle = $( element ).find( '.title > a' ).text().trim();
+        const placeTown = $( element ).find( '.place-location' ).text().trim();
+
+        const hide = (
+            ( town !== '' ) && ( town !== placeTown ) ) ||
+            ( ( name !== '' ) &&
+            ( placeTitle.toLowerCase().indexOf( name.toLowerCase() ) === -1 ));
+        if ( hide ) {
+            $( element ).hide();
+        } else {
+            $( element ).show();
+        }
+    } );
 });
